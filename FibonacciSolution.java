6c30b19fafc6124c2174c22fbc4fcafee419bf96
natalibaidoshvili12 <58605691+natalibaidoshvili12@users.noreply.github.com>
2020-03-20 00:07:25 +0400

    Update FibonacciSolution.java

--- FibonacciSolution.java
+++ FibonacciSolution.java
@@ -3,22 +3,24 @@ package Fibo;
 public class FibonacciSolution {
 
 	public long check(int i) {
-		long[] fib = new long[i];
+		long[] fib = new long[i + 1];
+		if (i <= 0)
+			return 0;
 		return check2(i, fib);
 
 	}
 
 	public long check2(int i, long fib[]) {
-		if (i <= 0)
-			return 0;
-		if (i == 1) {
-			fib[i - 1] = 0;
+		if (i <= 1) {
+			fib[i] = 0;
+			return fib[i];
 		} else if (i == 2) {
-			fib[i - 1] = 1;
+			fib[i] = 1;
+			return fib[i];
 		} else {
-			fib[i - 1] = check2(i - 1, fib) + check2(i - 2, fib);
+			fib[i] = check2(i - 1, fib) + check2(i - 2, fib);
 		}
-		return fib[i - 1];
+		return fib[i - 1] + fib[i - 2];
 	}
 
 }
